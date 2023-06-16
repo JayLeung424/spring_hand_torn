2aa52d2606b6e901300f3ba7e4ebe089c3ca6926
Jay <jay@JaydeMac-Studio.local>
2023-06-16 16:35:57 +0800

    spring-step-01 :创建简单的 Bean 容器

---
+++ src/test/java/com/spring/springframework/test/ApiTest.java
@@ -0,0 +1,8 @@
+package com.spring.springframework.test;/**
+ *@ClassName: ApiTest
+ *@Description: 
+ *@Author: jay
+ *@Date: 2023/6/16 16:29
+ **/
+public class ApiTest {
+}
---
+++ src/test/java/com/spring/springframework/test/bean/UserService.java
@@ -0,0 +1,10 @@
+package com.spring.springframework.test.bean;
+
+/**
+ * @ClassName: UserServiceTest
+ * @Description:
+ * @Author: jay
+ * @Date: 2023/6/16 16:28
+ **/
+public class UserService {
+}
